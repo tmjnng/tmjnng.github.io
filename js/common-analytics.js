db0bd870edc4dff70092244b6b43f6c19cca90bc
tanmengjiang <tanmengjiang@newcon.cn>
2026-02-02 10:57:03 +0800

    有百度统计代码都集中在一个共通文件中，便于管理和维护。当需要更新百度统计代码时，只需修改 js/common-analytics.js 文件即可，无需修改所有index.html文件

---
+++ js/common-analytics.js
@@ -0,0 +1,8 @@
+// 百度统计代码 - 共通引用文件
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?7dec0a7b14ff3afbf830639ed6da0d5c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
